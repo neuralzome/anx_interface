3a6409d411b0389ca2e2ed3a50b790b258963796
Aditya Shriwastava <aditya@flomobility.com>
2023-03-18 11:59:54 +0530

    Update perf_logger

--- anx_logging/perf_logger.py
+++ anx_logging/perf_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import signal
 from datetime import datetime
 import os
 import psutil
@@ -8,6 +9,8 @@ import numpy as np
 
 class PerfLogger:
     def __init__(self):
+        signal.signal(signal.SIGINT, self.signal_handler)
+
         self.thermal_zones = {
                 "cpu0-silver-usr": "thermal_zone11",
                 "cpu1-silver-usr": "thermal_zone22",
@@ -22,6 +25,8 @@ class PerfLogger:
         self.filepath = "/root/.logs/perflogs/"
         self.filename = f"{datetime.now().strftime('%d-%m-%Y-%H-%M-%S')}.csv"
 
+        self.ok = True
+
         self.thermals = []
         self.core_freq = []
         self.ram = 0
@@ -31,6 +36,9 @@ class PerfLogger:
         self.I = 0
 
         self.init_logfile()
+
+    def signal_handler(self, sig, frame):
+        self.ok = False
 
     def init_logfile(self):
         if not os.path.exists(self.filepath):
@@ -50,7 +58,7 @@ class PerfLogger:
             fd.write("\n")
 
     def logger_thread(self):
-            while True:
+            while self.ok:
                 with open(self.filepath + self.filename, 'a') as fd:
                     # 0. Log timestamp
                     fd.write(f"{time.time()},")
@@ -77,29 +85,30 @@ class PerfLogger:
                         fd.write(f"{freq},")
 
                     # 3. Log ram data
-                        self.ram = "{:.2f}".format(psutil.virtual_memory().used/(1024*1024)) # In MB
-                        fd.write(f"{self.ram},")
+                    self.ram = "{:.2f}".format(psutil.virtual_memory().used/(1024*1024)) # In MB
+                    fd.write(f"{self.ram},")
 
-                        self.gpu_percentage = 0
-                        self.V = 0
-                        self.I = 0
+                    self.gpu_percentage = 0
+                    self.V = 0
+                    self.I = 0
                     # 4. Log cpu percentage
-                        self.cpu_percentage = psutil.cpu_percent()
-                        fd.write(f"{self.cpu_percentage},")
+                    self.cpu_percentage = psutil.cpu_percent(1) # Blocking for 1s
+                    fd.write(f"{self.cpu_percentage},")
 
-                    # TODO: 5. Log gpu percentage
-                        self.gpu_percentage = 0
-                        fd.write(f"{self.gpu_percentage},")
+                    # 5. Log gpu percentage
+                    path = "/sys/class/kgsl/kgsl-3d0/gpu_busy_percentage"
+                    self.gpu_percentage = float(self.read(path).split(' ')[0])
+                    fd.write(f"{self.gpu_percentage},")
 
                     # 6. Log battery terminal input voltage
-                        path = "/sys/class/power_supply/battery/voltage_now"
-                        self.V = "{:.2f}".format(float(self.read(path))/1000)
-                        fd.write(f"{self.V},")
+                    path = "/sys/class/power_supply/battery/voltage_now"
+                    self.V = "{:.2f}".format(float(self.read(path))/1000)
+                    fd.write(f"{self.V},")
 
                     # 7. Log battery terminal input current
-                        path = "/sys/class/power_supply/battery/current_now"
-                        self.I = "{:.2f}".format(float(self.read(path))/1000)
-                        fd.write(f"{self.I}")
+                    path = "/sys/class/power_supply/battery/current_now"
+                    self.I = "{:.2f}".format(float(self.read(path))/1000)
+                    fd.write(f"{self.I}")
 
                     fd.write("\n")
 
@@ -109,8 +118,6 @@ class PerfLogger:
                 silver_core_avg_freq = "{:.2f}".format(np.array(self.core_freq[:4]).sum()/4)
                 gold_core_avg_freq = "{:.2f}".format(np.array(self.core_freq[4:8]).sum()/4)
                 print(f"Ts: {silver_core_avg_temp}°C, Tg: {gold_core_avg_temp}°C, Fs: {silver_core_avg_freq}MHz, Fg: {gold_core_avg_freq}MHz, ram: {self.ram}MB, cpu%: {self.cpu_percentage}, gpu%: {self.gpu_percentage}, V: {self.V}mV, I: {self.I}mA")
-
-                time.sleep(1)
 
     def read(self, path):
         with open(path, 'r') as fd:
